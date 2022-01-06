011ca824ab5421e6e88c198c475dd574cffaed4a
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-01-07 02:36:19 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import List from "./components/List";
+
+function App() {
+    return (
+        <div>
+            <List />
+        </div>
+    )
+}
+
+export default App
